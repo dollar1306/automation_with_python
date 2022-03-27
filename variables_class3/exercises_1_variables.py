ff117dda2a16b1c78afdd26f9ccc944051f59f03
dollar1306 <dollar1306@gmail.com>
2022-03-27 05:55:17 +0300

    Initial commit

---
+++ variables_class3/exercises_1_variables.py
@@ -0,0 +1,14 @@
+
+
+
+
+first_name = input("What your name?")
+
+last_name = input("What your family name?")
+
+age = input("What your age?")
+
+print("Hello " + first_name +" " + last_name)
+print("Your age is: ",age)
+
+
---
+++ variables_class3/var1.py
@@ -0,0 +1,15 @@
+
+
+
+a = 1
+
+print("address of a:" , id(a))
+print("value of:a ",a)
+
+b = a
+print("address of b:" , id(b))
+print("value of:b ",b)
+
+a = 2
+print("address of a:" , id(a))
+print("value of:a ",a)
